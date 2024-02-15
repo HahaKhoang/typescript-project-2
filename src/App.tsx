07658f7687f156dcef6f894b05c6510744e03db3
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-15 12:56:37 -0600

    Creates Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Input from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
+  function saveHandler(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
+
   return (
     <main>
-      <Container as={Button}>Click me</Container>
+      <Form onSave={saveHandler}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
