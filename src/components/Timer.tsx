1fdc324aedaa363ddda91f677468c634c7bb9a88
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-16 12:37:26 -0600

    Creates useEffect for progress bar

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,9 +1,33 @@
-import Container from './UI/Container.tsx';
+import { useEffect, useRef, useState } from "react";
+import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import Container from "./UI/Container.tsx";
 
-export default function Timer() {
+export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
+      <h2>{name}</h2>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
