480661f1350e5045c54291f8a25cfe963c04d026
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-16 11:05:37 -0600

    Creates useTimersContext custom hook

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useContext } from "react";
 
 type Timer = {
   name: string;
@@ -16,4 +16,34 @@ type TimersContextValue = TimersState & {
   stopTimers: () => void;
 };
 
-const TimersContext = createContext<TimersContextValue | null>(null);
+export const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error("TimersContext is null - that should not be the case!");
+  }
+
+  return timersCtx;
+}
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
